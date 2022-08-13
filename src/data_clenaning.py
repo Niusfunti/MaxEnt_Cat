f6f6dc60f49e96543b351977378a729d2a49e8cd
nfont <nfontanetg@hotmail.com>
2022-08-13 17:45:52 +0200

    parameters for MaxEnt software

--- src/data_clenaning.py
+++ src/data_clenaning.py
@@ -4,42 +4,58 @@ from trans2UTM import project
 #Which species
 esp = "falsiot"
 
-for num in range(3, 7):
-    # Read data from different files of falsiot.
-    df = pd.read_csv(f"../data/{esp}/distribucio_0{num}.csv")
+# Read data from different files of falsiot.
+df = pd.read_csv(f"../data/{esp}/Apusapus.csv", sep=";")
 
-    x_utm_list = []
-    y_utm_list = []
+# Change dtype
+df["Data_obs"] = pd.to_datetime(df["Data_obs"])
 
-    # Transform WGS84 to UTM 31N-ETRS89.
-    for l in range(0, len(df)):
-        long, lat = df.iloc[l, :]["x"], df.iloc[l, :]["y"]
-        z, l, x_utm, y_utm = project((long, lat))
-        x_utm_list.append(x_utm)
-        y_utm_list.append(y_utm)
+m_groups = df.groupby(df['Data_obs'].dt.strftime('%B'))
 
-    # Delete columns not used in MaxEnd.
-    df.drop('ID_GRAFIC', inplace=True, axis=1)
-    df.drop('Grup_taxon', inplace=True, axis=1)
-    df.drop('Data_obs', inplace=True, axis=1)
-    df.drop('MAPX', inplace=True, axis=1)
-    df.drop('MAPY', inplace=True, axis=1)
-    df.drop('N_indi', inplace=True, axis=1)
-    df.drop('N_obs', inplace=True, axis=1)
-    df.drop('Nom_cat', inplace=True, axis=1)
-    df.drop('x', inplace=True, axis=1)
-    df.drop('y', inplace=True, axis=1)
+df["month"] = df["Data_obs"].dt.month
 
-    # Add new coordinates to dataframe
-    df["x_UTM"] = x_utm_list
-    df["y_UTM"] = y_utm_list
-
-    df["Nom_cien"] = df["Nom_cien"].apply(lambda x: x.replace("Apus apus", f"Apus_apus_0{num}"))
-
-    # Add month in nom cien
+df["name_month"] = df.apply(lambda row: f"Apus_apus_{row['month']}", axis=1)
 
 
-    # Save dataframe in csv file.
-    df.to_csv(f"data/falsiot/distribucio_utm_0{num}.csv", index=False)
+df = df[(2 <= df["month"]) & (df["month"] <= 6)]
+
+
+# m_groups.groups.keys()
+df.reset_index()
+
+x_utm_list = []
+y_utm_list = []
+
+# Transform WGS84 to UTM 31N-ETRS89.
+for l in range(0, len(df)):
+    long, lat = df.iloc[l, :]["x"], df.iloc[l, :]["y"]
+    z, l, x_utm, y_utm = project((long, lat))
+    x_utm_list.append(x_utm)
+    y_utm_list.append(y_utm)
+
+# # Delete columns not used in MaxEnd.
+df.drop('Grup_taxon', inplace=True, axis=1)
+df.drop('Data_obs', inplace=True, axis=1)
+# df.drop('N_indi', inplace=True, axis=1)
+df.drop('N_obs', inplace=True, axis=1)
+df.drop('Nom_cat', inplace=True, axis=1)
+df.drop('Nom_cien', inplace=True, axis=1)
+df.drop('month', inplace=True, axis=1)
+df.drop('x', inplace=True, axis=1)
+df.drop('y', inplace=True, axis=1)
+#
+# # Add new coordinates to dataframe
+df["x_UTM"] = x_utm_list
+df["y_UTM"] = y_utm_list
+#
+# df["Nom_cien"] = df["Nom_cien"].apply(lambda x: x.replace("Apus apus", f"Apus_apus_0{num}"))
+#
+# # Add month in nom cien
+#
+#
+# # Save dataframe in csv file.
+
+df = df[["name_month", "x_UTM", "y_UTM", "N_indi"]]
+df.to_csv(f"data/falsiot/falsiot_map.csv", index=False)
 
 
