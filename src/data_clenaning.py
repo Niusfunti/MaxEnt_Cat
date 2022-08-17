578da08a0fbb02d9e072c7030d9b91bca813653d
nfont <nfontanetg@hotmail.com>
2022-08-17 13:28:20 +0200

    new csv

--- src/data_clenaning.py
+++ src/data_clenaning.py
@@ -2,10 +2,10 @@ import pandas as pd
 from trans2UTM import project
 
 #Which species
-esp = "abellerol"
+esp = "oreneta_v"
 
 # Read data from different files of species.
-df = pd.read_csv(f"../data/{esp}/abellerol.csv", sep=";")
+df = pd.read_csv(f"../data/{esp}/oreneta_v.csv", sep=";")
 
 # Change dtype
 df["Data_obs"] = pd.to_datetime(df["Data_obs"])
@@ -15,7 +15,7 @@ m_groups = df.groupby(df['Data_obs'].dt.strftime('%B'))
 df["month"] = df["Data_obs"].dt.month
 df["year"] = df["Data_obs"].dt.year
 
-df["name_month"] = df.apply(lambda row: f"abellerol_{row['month']}", axis=1)
+df["name_month"] = df.apply(lambda row: f"oreneta_v_{row['month']}", axis=1)
 
 
 df = df[(2 <= df["month"]) & (df["month"] <= 6)]
@@ -37,7 +37,7 @@ for l in range(0, len(df)):
 # # Delete columns not used in MaxEnd.
 df.drop('Grup_taxon', inplace=True, axis=1)
 df.drop('Data_obs', inplace=True, axis=1)
-df.drop('N_indi', inplace=True, axis=1)
+# df.drop('N_indi', inplace=True, axis=1)
 df.drop('N_obs', inplace=True, axis=1)
 df.drop('Nom_cat', inplace=True, axis=1)
 df.drop('Nom_cien', inplace=True, axis=1)
@@ -49,14 +49,14 @@ df.drop('y', inplace=True, axis=1)
 df["x_UTM"] = x_utm_list
 df["y_UTM"] = y_utm_list
 #
-# df["Nom_cien"] = df["Nom_cien"].apply(lambda x: x.replace("abellerol", f"abellerol_0{num}"))
+# # df["Nom_cien"] = df["Nom_cien"].apply(lambda x: x.replace("abellerol", f"abellerol_0{num}"))
 #
 # # Add month in nom cien
 #
 #
 # # Save dataframe in csv file.
 
-df = df[["name_month", "x_UTM", "y_UTM"]]
+df = df[["name_month", "x_UTM", "y_UTM", "N_indi"]]
 df.to_csv(f"data/abellerol/abellerol_maxent.csv", index=False)
 
 
